40db8dfe7ce97db9ef4ac8a3ecb36c1346caac81
Gopika <punni@suddenlink.net>
2019-02-09 12:16:32 -0500

    Changed data collections scripts so that they multiple collections can occur without overwriting

--- scripts/inital_data_collection/frames_recorder.py
+++ scripts/inital_data_collection/frames_recorder.py
@@ -10,24 +10,30 @@ import os
 
 class frames_recorder:
     def __init__(self):
-        rospy.Subscriber('/start', Bool, self.signal_cb)
+        rospy.Subscriber('/stop', Bool, self.signal_cb)
         rospy.Subscriber("/camera/color/image_raw", Image, self.image_cb)
         rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_cb)
         self.bridge = CvBridge()
         self.num_rgb = 0
         self.num_depth = 0
-        self.start = 0
+        self.num_collections = 0
+        self.stop = 1
+        self.stop_old = 0
 
     def signal_cb(self, msg):
-        self.start = msg.data
+        self.stop = msg.data
+        if ((self.stop - self.stop_old) == 1):
+            self.num_collections += 1
+        self.stop_old = self.stop
 
     def image_cb(self, data):
-        if (self.start == 1):
+        if (self.stop == 0):
             try:
                 image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             except CvBridgeError as e:
                 print e
             dir_rgb = rospy.get_param('~dir_rgb', '/home/gopika/collab-data/collab-rgb-frames')
+            dir_rgb = dir_rgb + "-collection-" + str(self.num_collections) 
             if not os.path.exists(dir_rgb):
                 os.makedirs(dir_rgb)
             file_rgb = dir_rgb + "/frame" + str(self.num_rgb) + '.png'
@@ -35,7 +41,7 @@ class frames_recorder:
             self.num_rgb += 1
     
     def depth_cb(self, data):
-        if (self.start == 1):
+        if (self.stop == 0):
             try:
                 image = self.bridge.imgmsg_to_cv2(data, "16UC1")
                 depth_array = np.array(image, dtype=np.float32)
@@ -43,6 +49,7 @@ class frames_recorder:
             except CvBridgeError as e:
                 print e
             dir_depth = rospy.get_param('~dir_depth' ,'/home/gopika/collab-data/collab-depth-frames')
+            dir_depth = dir_depth + "-collection-" + str(self.num_collections) 
             if not os.path.exists(dir_depth):
                 os.makedirs(dir_depth)
             file_depth = dir_depth + "/frame" + str(self.num_depth) + '.png'
--- scripts/inital_data_collection/voice_recorder.py
+++ scripts/inital_data_collection/voice_recorder.py
@@ -12,33 +12,40 @@ CHUNK = int(RATE / 10)  # 100ms
 
 class voice_recorder:
     def __init__(self):
-        rospy.Subscriber('/start', Bool, self.signal_cb)
-        self.start = 0
+        rospy.Subscriber('/stop', Bool, self.signal_cb)
+        self.stop = 1
+        self.stop_old = 0
         self.p = pyaudio.PyAudio()
         self.frames = []  
+        self.stop_recording = 0
+        self.num_collections = 0
 
     def signal_cb(self, msg):
-        self.start = msg.data
-        if (self.start == 1):
+        self.stop = msg.data
+        if ((self.stop - self.stop_old) == -1):
+            self.stop_old = self.stop
+            self.num_collections += 1
             self.start_recording()
-        elif (self.start == 0):
-            self.stop_recording()
+        elif ((self.stop - self.stop_old) == 1):
+            self.stop_old = self.stop
+            self.stop_recording()  
 
     def start_recording(self):
-        self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
-
-        while (self.start == 1):
-            data = self.stream.read(CHUNK)
+        self.stop_recording = 0
+        self.frames = []
+        stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+        
+        while self.stop_recording == 0:
+            data = stream.read(CHUNK)
             self.frames.append(data)
-            if (self.start == 0):
-                self.stop_recording()
-                break
 
-        self.stream.close()
-        dir_audio = "/home/gopika/collab-data/audio"
+        stream.close()
+        dir_audio = rospy.get_param('~dir_audio', '/home/gopika/collab-data/audio')
+        dir_audio = dir_audio + "-collection-" + str(self.num_collections) 
         if not os.path.exists(dir_audio):
             os.makedirs(dir_audio)
-        file_audio = dir_audio + "/recording.wav"
+
+        file_audio = dir_audio + "/recording" + str(self.num_collections) + ".wav"
         wf = wave.open(file_audio, 'wb')
         wf.setnchannels(1)
         wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
@@ -47,8 +54,7 @@ class voice_recorder:
         wf.close()
 
     def stop_recording(self):
-        self.start = 0
-
+        self.stop = 1
 
 def main():
     rospy.init_node("voice_recorder")
